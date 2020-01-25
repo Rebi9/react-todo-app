7945689d09f202522b976e1d64b714661a26c568
ryo_ebihara <ab.yr.2t@gmail.com>
2020-01-25 11:22:04 +0900

    feat:コンポーネント間でのデータの受け渡し

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Message from "./components/Message";
+import NameForm from "./components/NameForm";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <Message name="habu" />
-        <Message name="moriuchi" />
-        <Message name="sato" />
+        <NameForm name={name} onChangeName={value => setName(value)} />
+
+        <Message name={name} />
       </header>
     </div>
   );
