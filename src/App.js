8e395f64a49c6b69497ec0cbc166b4297288959f
ryo_ebihara <ab.yr.2t@gmail.com>
2020-01-30 07:48:30 +0900

    feat:ToDo追加機能の実装

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import uuid from "uuid";
 import "./App.css";
 import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -13,8 +15,18 @@ function App() {
     }
   ]);
 
+  const handleCreate = data => {
+    data.ID = uuid.v4;
+    const now = new Date().toISOString();
+    data.CreatedAt = now;
+    data.UpdatedAt = now;
+    setTodos([...todos, data]);
+  };
+
   return (
     <div className="App">
+      <TodoForm onSave={handleCreate} />
+
       {todos.map(item => (
         <Todo key={item.ID} {...item} />
       ))}
